07bd4d85f613dda287293e9deea529a0774cdb2e
Dany <dany21sayan_@hotmail.com>
2023-11-05 16:15:26 +0000

    add views, urls and templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import View
+
+from .models import Post
 
 # Create your views here.
+
+
+class BlogView(View):
+    def get(self, request):
+        all_posts = Post.objects.all()
+        print(all_posts)
+        return render(request, "home.html")
